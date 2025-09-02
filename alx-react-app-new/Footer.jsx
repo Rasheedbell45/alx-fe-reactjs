0ddfdef038c75fcaf413916dd2be739d09251590
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-02 06:41:38 +0100

    Create Footer.jsx

---
+++ alx-react-app-new/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        textAlign: "center",
+        padding: "10px",
+        marginTop: "20px",
+      }}
+    >
+      <p>© 2025 My Favorite Cities. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
