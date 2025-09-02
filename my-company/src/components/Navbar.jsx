2e25564122992480d3d60a9ab66fc19f5d2d72eb
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-02 08:06:57 +0100

    Update Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,11 +2,19 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ display: "flex", justifyContent: "space-between", padding: "10px" }}>
-      <a href="/">Home</a>
-      <a href="/about">About</a>
-      <a href="/services">Services</a>
-      <a href="/contact">Contact</a>
+   <nav style={{ display: "flex", justifyContent: "space-between", padding: "10px" }}>
+      <Link to="/" style={{ color: "white", margin: "0 15px" }}>
+        Home
+      </Link>
+      <Link to="/about" style={{ color: "white", margin: "0 15px" }}>
+        About
+      </Link>
+      <Link to="/services" style={{ color: "white", margin: "0 15px" }}>
+        Services
+      </Link>
+      <Link to="/contact" style={{ color: "white", margin: "0 15px" }}>
+        Contact
+      </Link>
     </nav>
   );
 }
