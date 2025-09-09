354fbd274edb8a7eb9035c49cf46caf9918d4192
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-09 06:46:17 +0100

    Create SearchBar.jsx

---
+++ recipe-sharing-app/src/components/SearchBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useRecipeStore } from '../store/recipeStore';
+
+const SearchBar = () => {
+  const setSearchTerm = useRecipeStore((state) => state.setSearchTerm);
+
+  return (
+    <input
+      type="text"
+      placeholder="Search recipes..."
+      onChange={(event) => setSearchTerm(event.target.value)}
+      className="border p-2 rounded w-full mb-4"
+    />
+  );
+};
+
+export default SearchBar;
