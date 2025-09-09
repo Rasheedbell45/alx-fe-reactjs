11057464434a69a1fd6717c22aa297865607cc67
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-09 06:20:01 +0100

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,1 +1,10 @@
-import { useRecipeStore } from '../store/recipeStore';
+import { create } from 'zustand';
+
+export const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (newRecipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, newRecipe]
+    })),
+  setRecipes: (recipes) => set({ recipes })
+}));
