97118204e1e66dd609fb215239b6ed616a9a3b3e
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-15 23:03:11 +0100

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,21 +2,21 @@ import axios from "axios";
 
 const BASE_URL = "https://api.github.com";
 const BASE_URL = "https://api.github.com/search/users?q";
+const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
 
-// Basic: Fetch a single user by username
+const headers = GITHUB_TOKEN ? { Authorization: `token ${GITHUB_TOKEN}` } : {};
+
 export const fetchUserData = async (username) => {
-  const response = await axios.get(`${BASE_URL}/users/${username}`);
+  const response = await axios.get(`${BASE_URL}/users/${username}`, { headers });
   return response.data;
 };
 
-// Advanced: Search users with filters (username, location, repo count)
 export const advancedUserSearch = async (username, location, minRepos) => {
   let query = "";
-
   if (username) query += `${username} in:login `;
   if (location) query += `location:${location} `;
   if (minRepos) query += `repos:>=${minRepos}`;
 
-  const response = await axios.get(`${BASE_URL}/search/users?q=${query}`);
+  const response = await axios.get(`${BASE_URL}/search/users?q=${query}`, { headers });
   return response.data;
 };
