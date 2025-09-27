83f2ac28ac1a5328cbde6030f627953669d973d9
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-27 14:45:48 +0100

    Create App.jsx

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,11 @@
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
