300ef197bfdfc0a129c4c1cd38616430bca64904
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-10-08 22:50:12 +0100

    Create BlogPost.jsx

---
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const BlogPost = () => {
+  const { id } = useParams();
+  return <h3>Viewing Blog Post #{id}</h3>;
+};
+
+export default BlogPost;
