92e06ce60a92d7e2d0e0166abc8a5e16d0d3fade
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-10-08 23:01:06 +0100

    Update ProfileDetails.jsx

--- react-router-advanced/src/components/ProfileDetails.jsx
+++ react-router-advanced/src/components/ProfileDetails.jsx
@@ -1,3 +1,12 @@
 import React from "react";
-const ProfileDetails = () => <p>User Details Section</p>;
+
+const ProfileDetails = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-semibold">Profile Details</h2>
+      <p>This section displays user details.</p>
+    </div>
+  );
+};
+
 export default ProfileDetails;
