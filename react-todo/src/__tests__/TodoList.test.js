a24303ef61807a036878c1581fe617d9adf37d67
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-10-09 13:35:25 +0100

    Create TodoList.test.js

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import TodoList from "../components/TodoList";
+
+describe("TodoList Component", () => {
+  test("renders initial todos", () => {
+    render(<TodoList />);
+    expect(screen.getByText("Learn React")).toBeInTheDocument();
+    expect(screen.getByText("Build Todo App")).toBeInTheDocument();
+  });
+
+  test("adds a new todo", () => {
+    render(<TodoList />);
+    const input = screen.getByPlaceholderText("Add new todo");
+    const addButton = screen.getByText("Add");
+
+    fireEvent.change(input, { target: { value: "Write Tests" } });
+    fireEvent.click(addButton);
+
+    expect(screen.getByText("Write Tests")).toBeInTheDocument();
+  });
+
+  test("toggles a todo's completed state", () => {
+    render(<TodoList />);
+    const todoItem = screen.getByText("Learn React");
+    fireEvent.click(todoItem);
+    expect(todoItem).toHaveStyle("text-decoration: line-through");
+  });
+
+  test("deletes a todo", () => {
+    render(<TodoList />);
+    const deleteButtons = screen.getAllByTestId("delete-button");
+    fireEvent.click(deleteButtons[0]);
+    expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
+  });
+});
